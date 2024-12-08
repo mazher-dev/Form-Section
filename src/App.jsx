67ceeb1a470241e73fb37c8ad0db3cfa130573b4
MH Code <mazheriftikhar1@gmail.com>
2024-12-08 14:13:18 +0500

    8-Dec change

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Form1 from './Components/Form1/Form1'
+import Form2 from './Components/Form2/Form2'
+import Form3 from './Components/Form3/Form3'
+import Form4 from './Components/Form4/Form4'
+import Form5 from './Components/Form5/Form5'
+import Form6 from './Components/Form6/Form6'
+import Form7 from './Components/Form7/Form7'
+import Form8 from './Components/Form8/Form8'
+import Form9 from './Components/Form9/Form9'
+import Form10 from './Components/Form10/Form10'
+import Form11 from './Components/Form11/Form11'
+import Form12 from './Components/Form12/Form12'
+import Form13 from './Components/Form13/Form13'
+import Form14 from './Components/Form14/Form14'
+import Form15 from './Components/Form15/Form15'
+import Form16 from './Components/Form16/Form16'
+import Form17 from './Components/Form17/Form17'
+import Form18 from './Components/Form18/Form18'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Form1 />
+      <Form2  />
+      <Form3 />
+      <Form4 />
+      <Form5 />
+      <Form6 />
+      <Form7 />
+      <Form8 />
+      <Form9 />
+      <Form10 />
+      <Form11 />
+      <Form12 />
+      <Form13 />
+      <Form14 />
+      <Form15 />
+      <Form16 />
+      <Form17 />
+      <Form18 />
+    </div>
   )
 }
 
